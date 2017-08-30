504b8d5689592598526e7e44dc1607e865416693
Chris Whatley <whatleyc@gmail.com>
2017-08-29 21:48:43 -0400

    Phantom downloader lambda

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-
 var AWS = require('aws-sdk');
 var phantomjs = require('phantomjs-prebuilt');
+const moment = require('moment');
+const fs = require('fs');
 
 exports.handler = function(event, context, callback) {
     console.log('I am on master');
-    var phantom = phantomjs.exec('phantomjs-script.js', 'arg1', 'arg2');
+    var url = decodeURIComponent(event.params.querystring.url);
+    var key = decodeURIComponent(event.params.querystring.key);
+    var id = event.params.querystring.ownerId || 0;
+    var output = id + '-' + key + '-' + moment().format('YYYYMMDD') + '-snapshot.png';
+    var size = event.size || '1200px';
+    var bucket = event.bucket || 'dipjar-kyc-repo';
+    var phantom = phantomjs.exec('rasterize.js', url , output, size);
 
     phantom.stdout.on('data', function(buf) {
         console.log('[STR] stdout "%s"', String(buf));
@@ -17,7 +24,30 @@ exports.handler = function(event, context, callback) {
     });
 
     phantom.on('exit', code => {
-        callback(null, 'fin!!');
+        var readStream = fs.createReadStream(output);
+        readStream.on('open', function() {
+            var s3obj = new AWS.S3({params: {Bucket: bucket}});
+            s3obj.upload({Body: readStream, Key: output}, function (err, data) {
+                console.log('AWS S3 Upload:', data);
+                callback(null, {
+                    key: key,
+                    id: id,
+                    inputURL: url,
+                    resultURL: data.Location,
+                    status: 'success'
+                });
+            });
+        });
+
+        readStream.on('error', function(e) {
+            console.log('Error reading rasterize output', e);
+            callback('Failed', {
+                key: key,
+                id: id,
+                url: url,
+                status: 'error'
+            });
+        });
     });
 
 };
--- test/basic.js
+++ test/basic.js
@@ -1,6 +1,6 @@
 // var assert = require('assert');
 
-describe('basic',function(){
+describe('basic', function(){
     this.timeout(60000);
     it('Should create', function(testDone){
         var event = require('./event.json');
@@ -18,7 +18,12 @@ describe('basic',function(){
         };
 
         var lambda = require("../index.js");
-        lambda.handler(event,context,testDone);
-        // assert(lambda);
+        lambda.handler(event,context, (err, result) => {
+            if (err) {
+                testDone(err);
+            } else {
+                testDone();
+            }
+        });
     });
 });
